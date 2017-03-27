b5e6710df5ad09deb4b9def1a39b5fb7a2a994a3
Jung Jo Hyung <epikjjh@gamil.com>
2017-03-27 12:38:40 +0900

    Proj1 Third commit : need to add quit check & trouble shooting

--- proj_shell/shell.c
+++ proj_shell/shell.c
@@ -2,7 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define MAX 100
+
 
 int Parse(char oper[], char oper_list[][MAX]); 
 void Execute(char oper[]);
@@ -12,18 +15,15 @@ int main(int argc, char *argv[]){
     char oper_list[MAX][MAX];
     int oper_num,i;
 
-    Initialize(oper, oper_list);
-
     if(argc == 1) {
         while (1) { 
             printf("prompt> ");
             fgets(oper, MAX, stdin);
             //quit check
             oper_num = Parse(oper, oper_list);
-            printf("%d", oper_num);
-           // for (i=0; i < oper_num; i++) {
-           //     Execute(oper_list[i]);
-           // }
+            for (i=0; i < oper_num; i++) {
+                Execute(oper_list[i]);
+            }
         }
      //Interactive mode
     } else if(argc == 2) {
@@ -53,25 +53,28 @@ int Parse(char oper[], char oper_list[][MAX]){
 
     token = strtok(oper, ";");
     while (token != NULL) {
-        strcmp(oper_list[i++],token);
+        strcpy(oper_list[i++], token);
         token = strtok(NULL, ";");
-    }
-    for(i=0;oper_list[i]!="\0";i++){
-        printf("%s",oper_list[i]);
     }
     
     return i;
 }
 void Execute(char oper[]){
-    int pid;
+    int pid, status, i=0;
     char *vector[MAX];
 
+    vector[i] = strtok(oper, " ");
+    if(vector[i] != NULL){
+        i++;
+        while((vector[i++] = strtok(NULL, " ")) != NULL);
+        vector[i] = NULL;
+    }
     pid = fork();
     if (pid == -1) {
         printf("Fork Error!\n");
     } else if(pid == 0){
-  //      execvp(oper_list,oper_list);
+        execvp(vector[0],vector);
     } else{
-        wait();
+        waitpid(pid, &status, 0);
     }
 }
